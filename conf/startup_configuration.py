91dae37bb0492b23de635bd1494df0dc5f15bf12
Alberto <bluesnatural@gmail.com>
2020-06-16 13:03:47 +0200

    added config setup and env logic

--- conf/startup_configuration.py
+++ conf/startup_configuration.py
@@ -7,6 +7,9 @@ from plantmonitor.task import task_counter_erp
 from plantmonitor.task import task_get_meteologica_forecast
 from plantmonitor.task import task_daily_upload_to_api_meteologica
 from plantmonitor.task import task_daily_download_to_api_meteologica
+
+from config import env, env_active
+
 def build_app():
     try:
 
@@ -24,6 +27,11 @@ def build_app():
 
 def add_jobs(app):
     logging.debug("Adding task")
-    app.add_job(task_counter_erp, 'interval', minutes=20)
-    app.add_job(task_daily_upload_to_api_meteologica, 'cron', kwargs={'test_env':True}, hour=18, minute=5)
-    app.add_job(task_daily_download_to_api_meteologica, 'cron', kwargs={'test_env':True}, hour=19, minute=5)
+    if env_active = env['in_plant']:
+        app.add_job(task, 'interval', minutes=5)
+    elif env_active = env['plantmonitor_server']:
+        app.add_job(task_counter_erp, 'interval', minutes=20)
+        app.add_job(task_daily_upload_to_api_meteologica, 'cron', kwargs={'test_env':True}, hour=18, minute=5)
+        app.add_job(task_daily_download_to_api_meteologica, 'cron', kwargs={'test_env':True}, hour=19, minute=5)
+    else:
+        logging.error("Environment not configured")
