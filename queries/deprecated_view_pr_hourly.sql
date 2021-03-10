f0e87e691d700a6e91114b5f8cdc60008c6ac827
Alberto <bluesnatural@gmail.com>
2021-03-10 17:03:55 +0100

    reordered fields for back2back

--- queries/deprecated_view_pr_hourly.sql
+++ queries/deprecated_view_pr_hourly.sql
@@ -1,13 +1,14 @@
  SELECT
-        CASE
-            WHEN (integrated_sensors.integral_irradiation_wh_m2 = 0) THEN (0.0)::double precision
-            WHEN ((1.05)::double precision < (((sistema_contador.export_energy)::double precision / (2160.0)::double precision) / (NULLIF((integrated_sensors.integral_irradiation_wh_m2)::double precision, (0.0)::double precision) / (1000000.0)::double precision))) THEN (1.0)::double precision
-            ELSE (((sistema_contador.export_energy)::double precision / (2160.0)::double precision) / (NULLIF((integrated_sensors.integral_irradiation_wh_m2)::double precision, (0.0)::double precision) / (1000000.0)::double precision))
-        END AS pr_hourly,
     timezone('Europe/Madrid'::text, sistema_contador."time") AS timezone,
-    sistema_contador.name AS contador
-   FROM public.integrated_sensors,
+    sistema_contador.name AS contador,
+    CASE
+        WHEN (integrated_sensors.integral_irradiation_wh_m2 = 0) THEN (0.0)::double precision
+        WHEN ((1.05)::double precision < (((sistema_contador.export_energy)::double precision / (2160.0)::double precision) / (NULLIF((integrated_sensors.integral_irradiation_wh_m2)::double precision, (0.0)::double precision) / (1000000.0)::double precision))) THEN (1.0)::double precision
+        ELSE (((sistema_contador.export_energy)::double precision / (2160.0)::double precision) / (NULLIF((integrated_sensors.integral_irradiation_wh_m2)::double precision, (0.0)::double precision) / (1000000.0)::double precision))
+    END AS pr_hourly
+FROM
+    public.integrated_sensors,
     public.sistema_contador
-  WHERE ((timezone('UTC'::text, sistema_contador."time") = integrated_sensors."time") AND (sistema_contador.name = '501600324'::text));
+WHERE ((timezone('UTC'::text, sistema_contador."time") = integrated_sensors."time") AND (sistema_contador.name = '501600324'::text));
 
 
