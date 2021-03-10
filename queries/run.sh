12e1973738a3b1a4606417eb3105acd306942642
Alberto <bluesnatural@gmail.com>
2021-03-10 17:05:07 +0100

    run.sh password also as env var

--- queries/run.sh
+++ queries/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Runs the sql file
 
-# Should export PGHOST PGPORT PGUSER PGDATABASE PGOPTIONS
+# Should export PGHOST PGPORT PGUSER PGDATABASE PGPASSWORD PGOPTIONS
 [ -e dbconfig.sh ] && source dbconfig.sh
 
 viewname=${1%.sql}
