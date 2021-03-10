2d417faf6070d663dbbbfe0229ee5c65f5df288c
Alberto <bluesnatural@gmail.com>
2021-03-10 18:10:57 +0100

    view_meter_export... reordered fields for better sorting

--- queries/view_meter_export_energy_daily.sql
+++ queries/view_meter_export_energy_daily.sql
@@ -1,6 +1,6 @@
  SELECT date_trunc('day'::text, sistema_contador."time") AS day_date,
-    (sum(sistema_contador.export_energy) * (1000)::numeric) AS export_energy_wh,
-    sistema_contador.name AS contador
+    sistema_contador.name AS contador,
+    (sum(sistema_contador.export_energy) * (1000)::numeric) AS export_energy_wh
    FROM public.sistema_contador
   GROUP BY (date_trunc('day'::text, sistema_contador."time")), sistema_contador.name
   ORDER BY (date_trunc('day'::text, sistema_contador."time"));
