f7575e2e440474f823575df9c7eb05613a56ee9a
Alberto <bluesnatural@gmail.com>
2021-03-11 13:34:38 +0100

    back2back.sh: properly reporting failures and success

--- queries/back2back.sh
+++ queries/back2back.sh
@@ -7,13 +7,15 @@ do
 	result="b2bdata/${base}-result.tsv"
 	expected="b2bdata/${base}-expected.tsv"
 	./run.sh "$query" | sort > "$result"
-	diff -q "${result}" "${expected}" || (
-		failed="$failed "
-	)
+	diff -q "${result}" "${expected}" || {
+		failed="$failed $base"
+	}
 done
 for a in $failed;
 do
-	echo a
+	echo -e "\033[31mFailed $a\033[0m"
 done
+
+[ "$failed" ] || echo -e "\033[32mAll passed\033[0m" 
 
 
