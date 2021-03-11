774ed63d0da462512050f355f613b58f70c78af5
Alberto <bluesnatural@gmail.com>
2021-03-11 18:11:44 +0100

    view_pr_hourly formatted

--- queries/view_pr_hourly.sql
+++ queries/view_pr_hourly.sql
@@ -7,8 +7,14 @@
             WHEN ((1.05)::double precision < raw_pr_tmp.pr_hourly_raw) THEN (1.0)::double precision
             ELSE raw_pr_tmp.pr_hourly_raw
         END AS pr_hourly
-   FROM (public.integrated_sensors
-     JOIN public.sistema_contador ON ((integrated_sensors."time" = timezone('UTC'::text, sistema_contador."time")))),
-    LATERAL ( VALUES ((((sistema_contador.export_energy)::double precision / (2160.0)::double precision) / (NULLIF((integrated_sensors.integral_irradiation_wh_m2)::double precision, (0.0)::double precision) / (1000000.0)::double precision)))) raw_pr_tmp(pr_hourly_raw);
+   FROM (
+     public.integrated_sensors
+     JOIN public.sistema_contador
+	ON integrated_sensors."time" = timezone('UTC'::text, sistema_contador."time")),
+    LATERAL (
+	VALUES (
+		((sistema_contador.export_energy)::double precision / (2160.0)::double precision) / (NULLIF((integrated_sensors.integral_irradiation_wh_m2)::double precision, (0.0)::double precision) / (1000000.0)::double precision)
+		)
+	) raw_pr_tmp(pr_hourly_raw);
 
 
