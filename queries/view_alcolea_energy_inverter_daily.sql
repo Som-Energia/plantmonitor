3759b9ed8c6dc98ad303b8a07b08bfaf9293921a
Alberto <bluesnatural@gmail.com>
2021-03-15 17:28:50 +0100

    alcolea_energy-inverter_daily reordered fields for b2b

--- queries/view_alcolea_energy_inverter_daily.sql
+++ queries/view_alcolea_energy_inverter_daily.sql
@@ -1,9 +1,10 @@
- SELECT date_trunc('day'::text, sistema_inversor."time") AS day_date,
-    (max(sistema_inversor.daily_energy_l) * 100) AS energy_inversor_wh,
-    sistema_inversor.inverter_name
+ SELECT 
+    date_trunc('day'::text, sistema_inversor."time") AS day_date,
+    sistema_inversor.inverter_name,
+    (max(sistema_inversor.daily_energy_l) * 100) AS energy_inversor_wh
    FROM public.sistema_inversor
   WHERE (sistema_inversor.location = 'Alcolea'::text)
   GROUP BY (date_trunc('day'::text, sistema_inversor."time")), sistema_inversor.inverter_name
-  ORDER BY (date_trunc('day'::text, sistema_inversor."time"));
+  ORDER BY (date_trunc('day'::text, sistema_inversor."time")), sistema_inversor.inverter_name;
 
 
