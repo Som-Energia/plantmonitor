84dc02c7ead16a50c7d9bd04d1d6cfee1d04ee96
Alberto <bluesnatural@gmail.com>
2021-03-15 18:20:48 +0100

    view_meter_(export_)energy_monthly column shuffle

--- queries/view_meter_energy_monthly.sql
+++ queries/view_meter_energy_monthly.sql
@@ -1,7 +1,9 @@
- SELECT (sum(sistema_contador.export_energy) * (1000)::numeric) AS export_energy_wh,
-    date_trunc('month'::text, sistema_contador."time") AS "time",
-    sistema_contador.name
-   FROM public.sistema_contador
-  GROUP BY sistema_contador.name, (date_trunc('month'::text, sistema_contador."time"));
+SELECT
+   date_trunc('month'::text, sistema_contador."time") AS "time",
+   sistema_contador.name,
+   (sum(sistema_contador.export_energy) * (1000)::numeric) AS export_energy_wh
+FROM public.sistema_contador
+GROUP BY (date_trunc('month'::text, sistema_contador."time")), sistema_contador.name
+ORDER BY (date_trunc('month'::text, sistema_contador."time")), sistema_contador.name;
 
 
--- queries/view_meter_export_energy_monthly.sql
+++ queries/view_meter_export_energy_monthly.sql
@@ -1,8 +1,9 @@
- SELECT date_trunc('month'::text, sistema_contador."time") AS month_date,
-    (sum(sistema_contador.export_energy) * (1000)::numeric) AS export_energy_wh,
-    sistema_contador.name AS contador
-   FROM public.sistema_contador
-  GROUP BY (date_trunc('month'::text, sistema_contador."time")), sistema_contador.name
-  ORDER BY (date_trunc('month'::text, sistema_contador."time"));
+SELECT
+   date_trunc('month'::text, sistema_contador."time") AS month_date,
+   sistema_contador.name AS contador,
+   (sum(sistema_contador.export_energy) * (1000)::numeric) AS export_energy_wh
+FROM public.sistema_contador
+GROUP BY (date_trunc('month'::text, sistema_contador."time")), sistema_contador.name
+ORDER BY (date_trunc('month'::text, sistema_contador."time")), sistema_contador.name;
 
 
