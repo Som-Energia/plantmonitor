75f2b64f9b8706a6ab58199a9d377945e710d248
Alberto <bluesnatural@gmail.com>
2021-03-30 15:25:11 +0200

    fixed log message when lastDate is None

--- plantmonitor/readings_facade.py
+++ plantmonitor/readings_facade.py
@@ -69,7 +69,7 @@ class ReadingsFacade():
 
   def getDeviceReadings(self, meterName, lastDate, upto=datetime.datetime.now(datetime.timezone.utc)):
 
-    logger.debug("Asking for measures for meter {} older than {} upto {} from erp to ponyorm".format(meterName, lastDate.strftime("%Y-%m-%d %H:%M:%S") if lastDate else None, upto.strftime("%Y-%m-%d %H:%M:%S")))
+    logger.debug("Asking for measures for meter {} older than {} upto {} from erp to ponyorm".format(meterName, None if not lastDate else lastDate.strftime("%Y-%m-%d %H:%M:%S"), upto.strftime("%Y-%m-%d %H:%M:%S")))
 
     measures = measures_from_date(
       self.client,
