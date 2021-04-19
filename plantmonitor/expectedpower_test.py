3eae1cdc8da7e4b030545a4340dab02951ed05a4
David García Garzón <david.garcia@somenergia.coop>
2021-04-19 14:10:16 +0200

    expectedpower_test: degradation from par (single faulure of a µW)

--- plantmonitor/expectedpower_test.py
+++ plantmonitor/expectedpower_test.py
@@ -207,7 +207,7 @@ class ExpectedPower_Test(TestCase):
                     1 +
                     (temperature_dc/10.0 - par.standard_conditions_temperature_dc/10.0) * par.voltage_temperature_coefficient_mpercent_c/1000.0/100.0
                 )
-                * $degradation/100.0
+                * par.degradation_cpercent/100.0/100.0
                 / 1000.0   -- W -> kW
             ) 
             END AS expectedpower
