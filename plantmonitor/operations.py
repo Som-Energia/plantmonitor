9baa4a1b59125a1ad6fc612af84a4be320dba881
quimnuss <quimnuss@petit>
2021-05-03 13:04:48 +0200

    raw sql instead of pony for views: integrate rewrite wihout pony

--- plantmonitor/operations.py
+++ plantmonitor/operations.py
@@ -65,6 +65,30 @@ def integrateHour(hourstart, query, dt=timedelta(hours=1)):
 
     return integralMetricValue
 
+def integrateHourFromTimeseries(hourstart, timeseries, dt=timedelta(hours=1)):
+
+    # for each hour within fromDate and toDate
+
+    hourend = hourstart + dt
+
+    # slice
+    timeSeriesSlice = [
+        (t, v)
+        for t,v in timeseries if hourstart <= t and t <= hourend
+    ]
+
+    if not timeSeriesSlice:
+        # logger.warning("No values in hour range")
+        return None
+
+    xvalues, yvalues = list(zip(*timeSeriesSlice))
+
+    # hourly_trapezoidal_approximation
+    integralMetricValueDateTime = integrate.trapz(y=yvalues, x=xvalues)
+    # trapz returns in x-axis type, so we need to convert the unreal datetime to the metric value
+    integralMetricValue = integralMetricValueDateTime.days * 24 + integralMetricValueDateTime.seconds // 3600
+
+    return integralMetricValue
 
 def integrateMetric(registries, fromDate, toDate):
     # for each hour within fromDate and toDate
@@ -118,28 +142,40 @@ def getTimeRange(sensor, srcregistry, dstregistry, srcCol, dstCol, fromDate=None
 
 # TODO refactor this function so it can be merged with integrateAllSensors above
 def integrateExpectedPower(fromDate=None, toDate=None):
+
     sensors = orm.select(sensor for sensor in SensorIrradiation)
+
     expectedPowerViewQuery = Path('queries/new/view_expected_power.sql').read_text(encoding='utf8')
-    srcregistry = expectedPowerViewQuery
-    srcCol = 'expectedPower'
-    dstregistry = HourlySensorIrradiationRegistry
+    srcCol = 'expectedpower'
     dstCol = 'expected_energy_wh'
+
+    expectedPowerRegs = database.select(expectedPowerViewQuery)
+
+    if not expectedPowerRegs:
+        logger.warning("No expectedPower registries. expected energy will not be computed.")
+        return None
 
     integratedMetric = {}
 
-    # Note how we use getattr to generalize which metric we select
-    # we need something to ensure the column (aka metric) exists
-
+    # TODO use fromDate. toDate
     for sensor in sensors:
-        metricFromDate, metricToDate = getTimeRange(sensor, srcregistry, dstregistry, srcCol, dstCol, fromDate, toDate)
+        metricFromDate = getOldestTime(sensor, HourlySensorIrradiationRegistry, dstCol)
+
+        sensorExpectedPower = [(r['time'],r[srcCol] )for r in expectedPowerRegs if r.sensor == sensor.name]
+        if not sensorExpectedPower:
+            logger.warning("No expectedPower for sensor {}. Skipping".format(sensor.name))
+        # TODO assuming ordered by time (it's in the query)
+        metricToDate = sensorExpectedPower[-1]
         if not metricFromDate:
-            logger.warning("sensor {} {} has no valid registries. skipping.".format(sensor.to_dict(), srcCol))
-            continue
-        devicesRegistries = orm.select((r.time, getattr(r, srcCol))
-            for r in srcregistry
-            if r.sensor == sensor and metricFromDate <= r.time and r.time <= metricToDate
-        )
-        integratedMetric[sensor] = integrateMetric(devicesRegistries, metricFromDate, metricToDate)
+            metricFromDate = sensorExpectedPower[0]
+
+        dt = timedelta(hours=1)
+        fromHourDate = fromDate.replace(minute=0, second=0, microsecond=0)
+        hours = [fromHourDate + i*dt for i in range(math.ceil((toDate - fromHourDate)/dt)) ]
+
+        # TODO sensorExpectedPower is a list, not a pony query, we have to rewrite evreything
+
+        integratedMetric[sensor] = [(hourstart + dt, integrateHourFromTimeseries(hourstart, sensorExpectedPower, dt)) for hourstart in hours]
 
     return integratedMetric
 
