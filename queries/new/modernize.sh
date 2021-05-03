27999a74505206bd9bf78d36ccb7630b6341ed5f
David García Garzón <david.garcia@somenergia.coop>
2021-05-03 10:32:45 +0200

    modernizing view_pr_hourly

--- queries/new/modernize.sh
+++ queries/new/modernize.sh
@@ -4,7 +4,7 @@ cat "$1" |
 sed 's%sistema_contador.name%meter.name%' | 
 sed 's%sistema_contador."time"%meterregistry."time"%' | 
 sed 's%sistema_contador.export_energy%meterregistry.export_energy_wh%' |
-sed 's%integrated_sensors.integral_irradiation_wh_m2%sensorirradiationregistry.irradiation_w_m2%' |
+sed 's%integrated_sensors.integral_irradiation_wh_m2%hourly_sensor_irradiation_registry.irradiation_wh_m2%' |
 sed 's%sistema_inversor."time"%inverterregistry."time"%' | 
 sed 's%sistema_inversor.inverter_name%inverter.name%' | 
 sed 's%sistema_inversor.daily_energy_l%inverterregistry.energy_wh%' |
