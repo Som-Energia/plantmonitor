49d97b36c43213e39b7061abfa6d9decc04caaca
quimnuss <quimnuss@petit>
2021-05-27 18:22:08 +0200

    use ht,hd daily only

--- queries/view_availability.sql
+++ queries/view_availability.sql
@@ -1,8 +1,11 @@
 SELECT
-    view_ht_monthly."time" AS "time",
-    view_ht_monthly.plant,
-    hd/ht AS "availability"
- FROM view_ht_monthly
- JOIN view_hd_monthly
-    ON view_hd_monthly.time = view_ht_monthly.time
-    and view_hd_monthly.plant = view_ht_monthly.plant;
+    date_trunc('month', view_ht_daily."time") AS "time",
+    view_ht_daily.plant,
+    sum(hd),
+    sum(ht),
+    sum(hd)::float/sum(ht) AS "availability"
+ FROM view_ht_daily
+ JOIN view_hd_daily
+    ON view_hd_daily.time = view_ht_daily.time
+    and view_hd_daily.plant = view_ht_daily.plant
+    group by date_trunc('month', view_ht_daily."time"), view_ht_daily.plant;
---
+++ queries/view_ht_daily.sql
@@ -0,0 +1,13 @@
+SELECT reg.sensor,
+       date_trunc('day', reg.time AT TIME ZONE 'Europe/Madrid') AS "time",
+       count(*) AS ht,
+       plant.name AS plant
+FROM plant
+INNER JOIN sensor ON sensor.plant = plant.id
+INNER JOIN hourlysensorirradiationregistry AS reg ON reg.sensor = sensor.id
+WHERE integratedirradiation_wh_m2 > 5
+  AND sensor.classtype = 'SensorIrradiation'
+GROUP BY date_trunc('day', reg.time AT TIME ZONE 'Europe/Madrid'),
+         reg.sensor,
+         plant.name
+ORDER BY date_trunc('day', reg.time AT TIME ZONE 'Europe/Madrid');
