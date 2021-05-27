720d8e9d096a5bb0b4950a3009140feb1e2126de
quimnuss <quimnuss@petit>
2021-05-27 18:33:12 +0200

    remove superfluos monthly hd ht

--- queries/view_hd_monthly.sql
+++
@@ -1,6 +0,0 @@
- SELECT date_trunc('month', "time" at time zone 'Europe/Madrid')  at time zone 'Europe/Madrid' AS "time",
-    plant,
-    count(*) AS hd
-   FROM view_pr_hourly
-  WHERE pr_hourly > 0.70
-  group by date_trunc('month', "time" at time zone 'Europe/Madrid'), plant;
