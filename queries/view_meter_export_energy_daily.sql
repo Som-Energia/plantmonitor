09dc017ffd172aab8e946aafc0770e2e513176b0
Alberto <bluesnatural@gmail.com>
2021-05-31 13:12:11 +0200

    view_meter_export_energy_daily fix units

--- queries/view_meter_export_energy_daily.sql
+++ queries/view_meter_export_energy_daily.sql
@@ -1,6 +1,6 @@
  SELECT date_trunc('day'::text, meterregistry."time") AS day_date,
     meter.name AS contador,
-    sum(meterregistry.export_energy_wh)*1000 AS export_energy_wh
+    sum(meterregistry.export_energy_wh) AS export_energy_wh
    FROM public.meter
    LEFT JOIN public.meterregistry ON meter.id = meterregistry.meter
   GROUP BY date_trunc('day'::text, meterregistry."time"), meter.name
