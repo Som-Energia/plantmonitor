122c987862789e9470a00530da61d874fbf886c1
Alberto <bluesnatural@gmail.com>
2021-06-03 10:55:40 +0200

    Fix date trunc timezone

--- queries/view_inverter_energy_monthly.sql
+++ queries/view_inverter_energy_monthly.sql
@@ -1,10 +1,10 @@
-SELECT date_trunc('month', TIME) AS TIME,
+SELECT date_trunc('month', "time" at time zone 'Europe/Madrid')  at time zone 'Europe/Madrid' AS "time",
        plant_id,
        plant_name,
        inverter_id,
        inverter_name,
        sum(energy_wh) as energy_wh
 FROM view_inverter_energy_daily AS daily
-GROUP BY date_trunc('month', time), plant_id, plant_name, inverter_id, inverter_name
-ORDER BY date_trunc('month', time), plant_id, plant_name, inverter_id, inverter_name
+GROUP BY date_trunc('month', "time" at time zone 'Europe/Madrid'), plant_id, plant_name, inverter_id, inverter_name
+ORDER BY date_trunc('month', "time" at time zone 'Europe/Madrid'), plant_id, plant_name, inverter_id, inverter_name
 ;
