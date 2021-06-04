f31dc70c4671fa145a8875c6ae666db4a18a5105
David García Garzón <david.garcia@somenergia.coop>
2021-06-04 11:34:25 +0200

    expectedpower: explicit using table prefix

--- queries/view_expected_power.sql
+++ queries/view_expected_power.sql
@@ -2,17 +2,17 @@ SELECT
     time,
     sensor.plant AS plant,
     sensor.id AS sensor,
-    CASE WHEN irradiation_w_m2 <=0 THEN 0 ELSE (
+    CASE WHEN reg.irradiation_w_m2 <=0 THEN 0 ELSE (
 	par.n_modules
 	* par.max_power_current_ma/1000.0
 	* par.max_power_voltage_mv/1000.0
 	* (
-	    irradiation_w_m2 / par.standard_conditions_irradiation_w_m2::float +
-	    (temperature_dc - par.standard_conditions_temperature_dc)/10.0 * par.current_temperature_coefficient_mpercent_c/100000.0
+	    reg.irradiation_w_m2 / par.standard_conditions_irradiation_w_m2::float +
+	    (reg.temperature_dc - par.standard_conditions_temperature_dc)/10.0 * par.current_temperature_coefficient_mpercent_c/100000.0
 	)
 	* (
 	    1 +
-	    (temperature_dc - par.standard_conditions_temperature_dc)/10.0 * par.voltage_temperature_coefficient_mpercent_c/100000.0
+	    (reg.temperature_dc - par.standard_conditions_temperature_dc)/10.0 * par.voltage_temperature_coefficient_mpercent_c/100000.0
 	)
 	* par.degradation_cpercent/10000.0 -- c% -> factor
 	* par.expected_power_correction_factor_cpercent/10000.0 -- c% -> factor
