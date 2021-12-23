8127be7fa73d5f27cba320ac79c4f9ed45e9801a
pol <pol.monso@somenergia.coop>
2021-12-23 14:54:55 +0100

    add postgres service and config

---
+++ conf/settings/testing_public.py
@@ -0,0 +1,14 @@
+from .base import *
+
+test_env = Env()
+test_env.read_env(os.path.join(BASE_DIR, '.env.testing_public'), override=True)
+
+DB_CONF = test_env.json('DATABASE_CONF')
+
+ACTIVEPLANT_CONF = test_env.json('PLANT_CONF')
+
+API_CONFIG = test_env.json('API_CONFIG')
+
+MONSOL = test_env.json('MONSOL')
+
+MONSOL_CREDENTIALS = test_env.json('MONSOL_CREDENTIALS')
