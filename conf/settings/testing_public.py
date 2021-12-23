c35176a902bcb6e3c7da64dc7eb7e63130180179
pol <pol.monso@somenergia.coop>
2021-12-23 14:58:07 +0100

    remove monsol from settings

--- conf/settings/testing_public.py
+++ conf/settings/testing_public.py
@@ -8,7 +8,3 @@ DB_CONF = test_env.json('DATABASE_CONF')
 ACTIVEPLANT_CONF = test_env.json('PLANT_CONF')
 
 API_CONFIG = test_env.json('API_CONFIG')
-
-MONSOL = test_env.json('MONSOL')
-
-MONSOL_CREDENTIALS = test_env.json('MONSOL_CREDENTIALS')
