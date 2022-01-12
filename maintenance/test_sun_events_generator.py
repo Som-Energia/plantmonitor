fabf74b657bd7b0eedec6e202dd5b512950f46f3
Alberto <bluesnatural@gmail.com>
2022-01-12 16:19:09 +0100

    Added test coverage SunEventsGenerator

--- maintenance/test_sun_events_generator.py
+++ maintenance/test_sun_events_generator.py
@@ -1,7 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import os
+from datetime import datetime, timedelta, timezone
+import ephem
+import unittest
 from .sun_events_generator import SunEventsGenerator
 
 
 # TODO: tests generate_sunevents
+
+class Sun_events_generator_Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def test_sampleSunEventsLatitude(self):
+
+        start = datetime(2022, 1, 12, 00, 00, 00, 0, tzinfo=timezone.utc)
+        end = datetime(2022, 1, 12, 23, 59, 00, 0, tzinfo=timezone.utc)
+        latitude = '41.9818'
+        longitude = '2.8237'
+        suneventsExpect = ephem.Observer()
+        suneventsExpect.lat = latitude
+        suneventsExpect.lon = longitude
+        sunevents = SunEventsGenerator(latitude, longitude)
+
+        self.assertEqual(sunevents.obs.lat, suneventsExpect.lat)
+
+    def test_sampleSunEventsLongitude(self):
+
+        start = datetime(2022, 1, 12, 00, 00, 00, 0, tzinfo=timezone.utc)
+        end = datetime(2022, 1, 12, 23, 59, 00, 0, tzinfo=timezone.utc)
+        latitude = '41.9818'
+        longitude = '2.8237'
+        suneventsExpect = ephem.Observer()
+        suneventsExpect.lat = latitude
+        suneventsExpect.lon = longitude
+        sunevents = SunEventsGenerator(latitude, longitude)
+
+        self.assertEqual(sunevents.obs.lon, suneventsExpect.lon)
