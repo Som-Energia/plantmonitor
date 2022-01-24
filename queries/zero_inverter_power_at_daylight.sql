774d60e9a0be3c61b2b283cdc7334d481cd6498a
pol <pol.monso@somenergia.coop>
2022-01-24 16:30:45 +0100

    add inverter trigger and alarm query

---
+++ queries/zero_inverter_power_at_daylight.sql
@@ -0,0 +1,12 @@
+
+select inverterregistry_5min_avg.time5min as time, plant.id as plant,
+    inverterregistry_5min_avg.inverter,
+    inverterregistry_5min_avg.power_w,
+    solarevent.sunrise as sunrise,
+    solarevent.sunset as sunset
+    from inverterregistry_5min_avg
+    left join inverter on inverter.id = inverterregistry_5min_avg.inverter
+    left join plant on plant.id = inverter.plant
+    left join solarevent on inverterregistry_5min_avg.time5min::date = solarevent.sunrise::date and solarevent.plant = plant.id
+    where power_w = 0 and inverterregistry_5min_avg.time5min between solarevent.sunrise and solarevent.sunset
+    order by time5min desc
