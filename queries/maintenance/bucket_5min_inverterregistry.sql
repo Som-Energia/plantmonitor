76c058c89810d23d47219de6271839481e82ebfe
pol <pol.monso@somenergia.coop>
2022-03-14 15:21:27 +0100

    change time_bucket to time_bucket_gapfill to ensure continuous timeseries (albeit with gaps)

--- queries/maintenance/bucket_5min_inverterregistry.sql
+++ queries/maintenance/bucket_5min_inverterregistry.sql
@@ -1,10 +1,10 @@
 SELECT
-   time_bucket('5 minutes', sir.time::timestamptz) as time,
+   time_bucket_gapfill('5 minutes', sir.time::timestamptz) as time,
    sir.inverter,
    avg(sir.temperature_dc)::int as temperature_dc,
    avg(sir.power_w)::int as power_w,
    max(sir.energy_wh)::bigint as energy_wh
 FROM inverterregistry as sir
-WHERE time between '{}' and '{}'
-GROUP by sir.inverter, time_bucket('5 minutes', sir.time::timestamptz)
+WHERE time >= '{}' and time < '{}'
+GROUP by sir.inverter, time_bucket_gapfill('5 minutes', sir.time::timestamptz)
 order by time desc
