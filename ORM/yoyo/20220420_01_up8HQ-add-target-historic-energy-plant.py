e5be99945eb79f0d90c9351c29c9f3d899d004c1
Alberto <bluesnatural@gmail.com>
2022-04-26 09:25:06 +0200

    Change name table plantestimatedmonthlyenergy

--- ORM/yoyo/20220420_01_up8HQ-add-target-historic-energy-plant.py
+++ ORM/yoyo/20220420_01_up8HQ-add-target-historic-energy-plant.py
@@ -1,5 +1,5 @@
 """
-Add plantexpectedmonthlyenergy table
+Add plantestimatedmonthlyenergy table
 """
 
 from yoyo import step
@@ -9,7 +9,7 @@ __depends__ = {'20211213_01_m7M7G-solar-events', '20220413_01_VgHU3-change-plant
 
 steps = [
     step('''
-	CREATE TABLE "plantexpectedmonthlyenergy" (
+	CREATE TABLE "plantestimatedmonthlyenergy" (
 	  "id" SERIAL PRIMARY KEY,
 	  "plantparameters" INTEGER NOT NULL,
 	  "time" TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -17,11 +17,11 @@ steps = [
 	  "monthly_historic_energy_kwh" BIGINT NOT NULL
 	);
 
-	CREATE INDEX "idx_plantexpectedmonthlyenergy__plantparameters" ON "plantexpectedmonthlyenergy" ("plantparameters");
+	CREATE INDEX "idx_plantestimatedmonthlyenergy__plantparameters" ON "plantestimatedmonthlyenergy" ("plantparameters");
 
-	ALTER TABLE "plantexpectedmonthlyenergy" ADD CONSTRAINT "fk_plantexpectedmonthlyenergy__plantparameters" 
-	      FOREIGN KEY ("plantparameters") REFERENCES "plantparameters" ("id");
+        ALTER TABLE "plantestimatedmonthlyenergy" ADD CONSTRAINT "fk_plantestimatedmonthlyenergy__plantparameters" 
+              FOREIGN KEY ("plantparameters") REFERENCES "plantparameters" ("id");
         ''','''
-        DROP TABLE "plantexpectedmonthlyenergy";
+        DROP TABLE "plantestimatedmonthlyenergy";
         ''')
 ]
