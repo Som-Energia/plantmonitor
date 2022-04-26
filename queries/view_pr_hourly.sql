5ee4ab649abd66d614e1c835d7902c6ac9915a15
pol <pol.monso@somenergia.coop>
2022-04-26 12:59:29 +0200

    Use plantparameters on view_pr_hourly

--- queries/view_pr_hourly.sql
+++ queries/view_pr_hourly.sql
@@ -4,7 +4,7 @@
     meter.name as meter,
     export_energy_wh,
     irradiation_w_m2_h,
-    (export_energy_wh / 2160000.0) / (NULLIF(irradiation_w_m2_h, 0.0) / 1000.0) AS pr_hourly
+    (export_energy_wh / plantparameters.peak_power_w::float) / (NULLIF(irradiation_w_m2_h, 0.0) / 1000.0) AS pr_hourly
  FROM meterregistry
  JOIN meter
     ON meterregistry.meter = meter.id
@@ -14,4 +14,6 @@
     ON sensor.plant = meter.plant
     AND view_irradiation.sensor = sensor.id
  JOIN plant
-    ON meter.plant = plant.id;
+    ON meter.plant = plant.id
+ LEFT JOIN plantparameters
+    on plant.id = plantparameters.plant;
