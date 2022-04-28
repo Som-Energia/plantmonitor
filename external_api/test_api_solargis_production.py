f4501c2890f4e3fa733a81d96623d767611aa50f
pol <pol.monso@somenergia.coop>
2022-04-28 17:38:30 +0200

    add test with production credentials

--- external_api/test_api_solargis_production.py
+++ external_api/test_api_solargis_production.py
@@ -2,10 +2,13 @@ import os
 os.environ.setdefault('PLANTMONITOR_MODULE_SETTINGS', 'conf.settings.devel')
 import unittest
 
-from external_api.api_solargis import ApiSolargis
+from external_api.api_solargis import ApiSolargis, Site
 
 from meteologica.utils import todtaware
 
+# run with pytest external_api.test_api_solargis_production
+# to ensure this script sets the PLANTMONITOR_MODULE_SETTINGS to devel
+# before any other test module
 @unittest.skipIf(True, "we're querying an external api, let's disable tests by default")
 class ApiSolargis_ProductionTest(unittest.TestCase):
 
@@ -16,6 +19,7 @@ class ApiSolargis_ProductionTest(unittest.TestCase):
         solargis_conf = envinfo.SOLARGIS
 
         cls.api = ApiSolargis(**solargis_conf)
+        cls.api.create_xsd_schema()
 
     def setUp(self):
         pass
@@ -23,22 +27,27 @@ class ApiSolargis_ProductionTest(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test__is_production(self):
+        self.assertNotEqual(self.api.api_key, 'demo')
+
     def test__get_current_solargis_irradiance_readings__production_credentials(self):
         self.maxDiff = None
 
-        from_date = todtaware('2021-11-01 13:00:00')
-        to_date = todtaware('2021-11-01 15:00:00')
+        from_date = todtaware('2022-04-01 13:00:00')
+        to_date = todtaware('2022-04-01 15:00:00')
 
-        lat=48.61259
-        long=20.827079
+        self.api.sites = {
+            3: Site(
+                id=3,
+                name='Fontivsolar',
+                latitude=40.932389,
+                longitude=-4.968694,
+                peak_power_w=990,
+                installation_type='FREE_STANDING',
+            )
+        }
 
-        status, result = self.api.get_current_solargis_irradiance_readings(lat, long, from_date, to_date)
+        readings = self.api.get_current_solargis_irradiance_readings(from_date, to_date)
 
-        self.assertEqual(status, 200)
-
-        # expected = self.sample_instant_reading()
-
-        self.assertEqual(status, 200)
-        self.assertTrue('data' in result)
-        # self.assertEqual(len(readings['data']), 1)
+        self.assertEqual(len(readings), 24)
         # self.assertListEqual(list(readings['data'][0].keys()), list(expected['data'][0].keys()))
