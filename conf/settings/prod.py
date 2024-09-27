c7f33163b62b5b59b6cbb716f47e1405e47c6fd3
Diego Quintana <daquintanav@gmail.com>
2024-09-27 09:35:08 -0300

    new: add prod settings to match examples

---
+++ conf/settings/prod.py
@@ -0,0 +1,16 @@
+from .base import *
+
+prod_env = Env()
+prod_env.read_env(os.path.join(BASE_DIR, ".env.prod"), override=True)
+
+# DATABASE configuration
+DB_CONF = prod_env.json("DATABASE_CONF")
+
+ACTIVEPLANT_CONF = prod_env.json("PLANT_CONF")
+
+API_CONFIG = prod_env.json("API_CONFIG")
+
+# MONSOL = prod_env.json('MONSOL')
+# MONSOL_CREDENTIALS = prod_env.json('MONSOL_CREDENTIALS')
+
+SOLARGIS = prod_env.json("SOLARGIS")
